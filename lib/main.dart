5dacdf295c305f69ae729420a57508250c963a98
KishanSoni00 <119392579+KishanSoni00@users.noreply.github.com>
2023-05-09 14:09:21 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Home_Page.dart';
+import 'package:flutter_application_1/pages/Home_Page.dart';
+import 'package:flutter_application_1/pages/Login_Page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/home": (context) => HomePage(),
+        "/": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
