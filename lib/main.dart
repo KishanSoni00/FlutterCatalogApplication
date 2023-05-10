88b01845f811127ee9663cb7e4b20c9bcaac2582
KishanSoni00 <119392579+KishanSoni00@users.noreply.github.com>
2023-05-10 19:04:23 +0530

    day3commited

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_application_1/pages/Home_Page.dart';
 import 'package:flutter_application_1/pages/Login_Page.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -18,16 +19,17 @@ class MyApp extends StatelessWidget {
       // home: HomePage(),
       themeMode: ThemeMode.light,
       theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          //primaryTextTheme: GoogleFonts.latoTextTheme()
-          ),
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        //primaryTextTheme: GoogleFonts.latoTextTheme()
+      ),
+      // debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/login",
       routes: {
-        "/home": (context) => HomePage(),
         "/": (context) => LoginPage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
